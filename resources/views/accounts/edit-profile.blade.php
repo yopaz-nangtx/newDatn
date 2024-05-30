2cf670d972cd9a04b89fd5a96697c67b3fd6821a
NangTX <nang.tx@tinasoft.com.vn>
2024-05-30 15:43:00 +0700

    fixing edit profile

---
+++ resources/views/accounts/edit-profile.blade.php
@@ -0,0 +1,82 @@
+
+@extends('layouts.master')
+@section('content')
+    <div class="page-wrapper">
+        <div class="content container-fluid">
+
+            <div class="page-header">
+                <div class="row align-items-center">
+                    <div class="col-sm-12">
+                        <div class="page-sub-header">
+                            <h3 class="page-title">Edit Profiles</h3>
+                            <ul class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="{{ route('room/add/page') }}">Profile</a></li>
+                                <li class="breadcrumb-item active">Edit Profiles</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- message --}}
+            {!! Toastr::message() !!}
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card comman-shadow">
+                        <div class="card-body">
+                            <form action="{{ route('room/add/save') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h5 class="form-title room-info">User Profile
+                                            <span>
+                                                <a href="javascript:;"><i class="feather-more-vertical"></i></a>
+                                            </span>
+                                        </h5>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label>Name <span class="login-danger">*</span></label>
+                                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" placeholder="Enter Name" value="{{ old('name') }}">
+                                            @error('name')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label>Capacity (Person) <span class="login-danger">*</span></label>
+                                            <input type="text" class="form-control @error('capacity') is-invalid @enderror" name="capacity" placeholder="Enter Capacity" value="{{ old('capacity') }}">
+                                            @error('capacity')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-sm-4">
+                                        <div class="form-group local-forms">
+                                            <label>Description</label>
+                                            <input type="text" class="form-control @error('description') is-invalid @enderror" name="description" placeholder="Enter Description" value="{{ old('description') }}">
+                                            @error('description')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="room-submit">
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
